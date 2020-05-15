e3bc80c41cd59a630fbbb054d245b08c7cb876ed
Ben <bengraves2000@gmail.com>
2020-05-15 21:39:07 +0100

    Added the custom messages section

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,12 @@
 import Cookies from 'js-cookie'
 
+const currentDate = new Date();
+const lockdownStart = new Date('03/23/2020');
+const nextReview = new Date('05/28/2020');
+
+var daysSinceStart = Math.ceil(Math.abs(currentDate - lockdownStart) / (1000 * 60 * 60 * 24));
+var daysUntilReview = Math.ceil(Math.abs(nextReview - currentDate) / (1000 * 60 * 60 * 24));
+
 //Set the mode initally
 var initMode = Cookies.get('mode');
 if(initMode != null){
@@ -8,6 +15,13 @@ if(initMode != null){
   // Set the correct link text
   var link = $("a[data-mode=" + initMode + "]")
   link.text(link.attr("data-off"));
+}
+
+var initCustom = Cookies.get('customMessages');
+if(initCustom != null){
+  setCustomMessages(JSON.parse(initCustom));
+}else{
+  setDefaultMessages();
 }
 
 // Atticbay page view
@@ -27,9 +41,6 @@ $("#darkLink").click(function () {
 	console.log("clicked");
 });
 
-const currentDate = new Date();
-const lockdownStart = new Date('03/23/2020');
-const nextReview = new Date('05/28/2020');
 setTimeout(function(){
 	$('#daysSince').text(Math.ceil(Math.abs(currentDate - lockdownStart) / (1000 * 60 * 60 * 24)));
 	$('#govReview').text(Math.ceil(Math.abs(nextReview - currentDate) / (1000 * 60 * 60 * 24)));
@@ -61,10 +72,66 @@ $("a[data-mode]").click(function () {
   toggleMode($(this).attr("data-mode"), $(this));
 })
 
-$("#more-settings").click(function (){
+$(".more-settings").click(function (){
   var defaultTxt = "more-settings";
-
-  $(this).text() = (defaultTxt ?
 
   $("#settings-section").toggleClass("hide");
 })
+
+$("#set-messages").click(function (){
+  // Please please clean first
+
+  var customMessages =
+  {
+    "dayMessage": stripHtml($("#custom-day-message").val()),
+    "govMessage": stripHtml($("#custom-gov-message").val())
+  };
+
+  // Set the messages cookie
+  Cookies.set('customMessages', JSON.stringify(customMessages), { expires: 365 });
+
+  console.log("the text is" + $("#custom-day-message").text())
+
+  // Run the method to update the messages
+  setCustomMessages(customMessages);
+})
+
+$("#reset-messages").click(function(){
+  Cookies.remove('customMessages');
+  setDefaultMessages()
+});
+
+function setDefaultMessages(){
+  // Set the messages on the page
+  $("#daysSinceText").html('Day ' + '<span id="daysSince" class="odometer">' + daysSinceStart + '</span>')
+  $("#reviewDaysText").html('Next government review in ' + '<span id="daysSince" class="odometer">' + daysUntilReview + '</span> days')
+}
+
+function setCustomMessages(customMessages){
+  var dayMessage = customMessages["dayMessage"];
+  var govMessage = customMessages["govMessage"];
+
+  // Set the inputs
+  $("#custom-day-message").text(dayMessage);
+  $("#custom-gov-message").text(govMessage);
+
+  if (dayMessage.includes("$d")){
+    dayMessage = dayMessage.replace('$d', '<span id="daysSince" class="odometer">' + daysSinceStart + '</span>')
+    $("#daysSinceText").html(dayMessage);
+    $("#day-message-validation").addClass("hide");
+  }else{
+    $("#day-message-validation").removeClass("hide");
+  }
+
+  if (govMessage.includes("$d")){
+    govMessage = govMessage.replace('$d', '<span id="daysSince" class="odometer">' + daysUntilReview + '</span>')
+    $("#reviewDaysText").html(govMessage);
+    $("#gov-message-validation").addClass("hide");
+  }else{
+    $("#gov-message-validation").removeClass("hide");
+  }
+}
+
+function stripHtml(htmlString){
+  return htmlString.replace(/<[^>]+>/g, '');
+}
